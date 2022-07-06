bb98d5803bb96c2248eeea47861682e58b32b5bf
Stéfany Larissa <stelar.dev@gmail.com>
2022-07-06 19:21:30 -0300

    working links

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-function route(event) {
-  event = event || window.event
-  handle();
+import { Router } from "./router.js";
 
-  event.preventDefault();
-}
+const router = new Router;
+
+router.add('/', '/pages/home.html');
+router.add(404, '/pages/404.html');
+router.add('/about', '/pages/about.html');
+router.add('/exploring', '/pages/exploring.html');
+router.add('/the-universe', '/pages/the-universe.html');
+
+router.handle();
+
+window.route = () => router.route()
+window.onpopstate = () => router.handle()
